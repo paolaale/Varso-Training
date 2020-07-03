33c4aad34ae869bf8aaadd1cacebdfa78cfd4b3d
Paola Villarreal García <31547279+paolaale@users.noreply.github.com>
2020-07-03 11:26:02 -0500

    starting week 2, learning about Vue.js

---
+++ Week 2/test/src/App.vue
@@ -0,0 +1,51 @@
+<template>
+  <div id="app">
+    <Todos v-bind:todos="todos" />
+  </div>
+</template>
+
+<script>
+
+import Todos from './components/Todos';
+
+export default {
+  name: 'App',
+  components: {
+    Todos
+  },
+  data() {
+    return {
+      todos: [
+        {
+          id: 1,
+          title: "To do one",
+          completed: false
+        },
+        {
+          id: 2,
+          title: "To do two",
+          completed: true
+        },
+        {
+          id: 3,
+          title: "To do three",
+          completed: false
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+
+body {
+  font-family: Arial, Helvetica, sans-serif;
+  line-height: 1.4;
+}
+</style>
