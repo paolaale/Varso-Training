40c922c0b782f19980316803ff795babb304f2fa
Paola Villarreal García <31547279+paolaale@users.noreply.github.com>
2020-07-13 09:59:55 -0500

    starting week 3 learning about typescript

---
+++ Week 3/typescript/src/components/HelloWorld.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <h2>{{ fName }}</h2>
+    <h2>{{ lName }}</h2>
+    <h2>{{ fullName }}</h2>
+    <h4> {{counter}} </h4>
+    <button @click="incrementCounter()">Press me</button>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class HelloWorld extends Vue {
+  @Prop() private msg!: string;
+  fName="Jane";
+  lName="Doe";
+  counter = 0;
+
+  get fullName(): string {
+    return this.fName + " " + this.lName
+  }
+
+  incrementCounter() {
+    this.counter++;
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
