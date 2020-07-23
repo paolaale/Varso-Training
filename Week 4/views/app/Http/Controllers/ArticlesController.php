b0afe82fa0b1c3006334717142faa3c5a12244c1
Paola Villarreal García <31547279+paolaale@users.noreply.github.com>
2020-07-23 11:45:33 -0500

    learning about request

--- Week 4/views/app/Http/Controllers/ArticlesController.php
+++ Week 4/views/app/Http/Controllers/ArticlesController.php
@@ -14,10 +14,42 @@ class ArticlesController extends Controller
         return view('articles.index', ['articles' => $articles]);
     }
 
-    public function show($id) {
-        
-        $article = Article::find($id);
+    public function show(Article $article) {
 
         return view('articles.show', ['article' => $article]);
+    }
+
+    public function create() {
+        
+        return view('articles.create');
+    }
+
+    public function store() {
+
+        Article::create($this->validateArticle());
+
+        return redirect('/articles');
+
+    }
+
+    public function edit(Article $article) {
+
+        return view('articles.edit', ['article' => $article]);
+    }
+
+    public function update(Article $article) {
+
+        $article->update($this->validateArticle());
+
+        return redirect($article->path());
+    }
+
+    protected function validateArticle() {
+
+        return request()->validate([
+            'title' => 'required',
+            'excerpt' => 'required',
+            'body' => 'required'
+        ]);
     }
 }
--- Week 4/views/resources/views/articles/index.blade.php
+++ Week 4/views/resources/views/articles/index.blade.php
@@ -7,13 +7,14 @@
             @foreach ($articles as $article)
                 <div class="content">
                     <div class="title">
-                        <h2><a href="/articles/{{ $article->id }}">{{ $article->title }}</a></h2>
+                        <h2><a href="{{ route('articles.show', $article->id) }}">{{ $article->title }}</a></h2>
                     </div>
                     <p>
                         <img src="/images/banner.jpg" class="image image-full">
                     </p>
                     {!! $article->excerpt !!}
                 </div>
+                <br>
             @endforeach
         </div>
     </div>
