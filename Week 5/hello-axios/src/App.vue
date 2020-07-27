eaeab9ee1f8e01f30df873b34e5075ed934ea3f7
Paola Villarreal García <31547279+paolaale@users.noreply.github.com>
2020-07-27 13:15:22 -0500

    learning about axios and vuex

---
+++ Week 5/hello-axios/src/App.vue
@@ -0,0 +1,62 @@
+<template>
+  <div id="app">
+    <h1>Todos</h1>
+    <input type="text" v-model="todoName" @keyup.enter="addTodo">
+    <ul>
+      <li v-for="todo of todos" :key="todo.id">
+        {{ todo.name }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+const baseURL = "http://localhost:3000/todos";
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      todos: [],
+      todoName: ''
+    };
+  },
+  async created() {
+    try {
+      const res = await axios.get(baseURL);
+      this.todos = res.data;
+    } catch (e) {
+      console.error(e);
+    }
+  },
+  methods: {
+    async addTodo() {
+      const res = await axios.post(baseURL, { name: this.todoName });
+      this.todos = [...this.todos, res.data];
+      this.todoName = '';
+    }
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+ul {
+  margin: 0px;
+  padding: 0px;
+}
+
+li {
+  list-style: none;
+}
+
+</style>
